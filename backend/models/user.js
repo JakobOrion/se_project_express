f69d1627a563889290dfabd1efc4b82ed321c672
Jake Bohn <jakoborion@gmail.com>
2023-06-05 03:41:12 -0400

    Add email and password to user schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,22 +1,20 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const { linkRegex } = require('../utils/utils');
 
 const userSchema = new mongoose.Schema({
-  // username
   name: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 30,
   },
-  // user information
   about: {
     type: String,
     required: true,
     minlength: 2,
     maxlength: 30,
   },
-  // link to the avatar
   avatar: {
     type: String,
     validate: {
@@ -25,6 +23,21 @@ const userSchema = new mongoose.Schema({
       },
     },
     required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
   },
 });
 
